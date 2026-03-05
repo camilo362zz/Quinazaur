18fa93761ff8777ce1e20051ecf8d1791d8c1f60
Sebas <sebasbenavides77@gmail.com>
2026-03-04 23:47:53 -0500

    Mejoré  conexion DB y el botón ver más

--- quinazaur_website/backend/app.py
+++ quinazaur_website/backend/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from __init__ import BD_CONFIG
 from bd_postgres import bd_postgres
-db=bd_postgres(**BD_CONFIG)
+
 
 app=Flask(__name__,template_folder="../templates", static_folder="../static")
 @app.route("/")
@@ -10,13 +10,17 @@ def inicio():
 
 @app.route("/productos")
 def productos():
+    db=bd_postgres(**BD_CONFIG)
     productos=db.select_productos()
     cant_prod=db.cant_productos()
+    db.disconnect()
     return render_template("productos.html",productos=productos, cantidad=cant_prod)
 
 @app.route("/recetas")
 def recetas():
+    db=bd_postgres(**BD_CONFIG)
     recetas=db.select_recetas()
+    db.disconnect()
     return render_template("recetas.html",recetas=recetas)
 
 if __name__=="__main__":
