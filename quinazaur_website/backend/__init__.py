51acd182d9a52c9eb25711e21b34003ef4c1c54d
Sebas <sebasbenavides77@gmail.com>
2026-03-06 23:56:37 -0500

    Eliminé __init__

--- quinazaur_website/backend/__init__.py
+++
@@ -1,7 +0,0 @@
-BD_CONFIG={
-    'host':'aws-0-us-west-2.pooler.supabase.com',
-    'port':'5432',
-    'database':'postgres',
-    'user':'postgres.fyfjkhtidrpbodbxghzt',
-    'password':'*QuinazauR_0706*'
-}
